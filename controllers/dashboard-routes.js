ea5043f57d46ebede30c7cbabfcc2aec66f8e1df
Matt Bush <59295261+matthewbush55@users.noreply.github.com>
2021-09-29 12:36:53 -0600

    Fixed edit post from dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -42,33 +42,42 @@ router.get("/new", (req, res) => {
   res.render("new-post");
 });
 
+//TODO: Fix withAuth
 // GET route to edit an existing post
-router.get("/edit/:id", withAuth, async (req, res) => {
-  try {
-    Post.findOne({
-      where: {
-        user_id: req.params.id,
+router.get("/edit/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "contents", "created_at"],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
       },
-      attributes: ["id", "title", "contents", "created_at"],
-      include: [
-        {
-          model: Comment,
-          attributes: ["id", "comment", "post_id", "user_id", "created_at"],
-          include: {
-            model: User,
-            attributes: ["username"],
-          },
-        },
-        {
+      {
+        model: Comment,
+        attributes: ["id", "comment", "post_id", "user_id", "created_at"],
+        include: {
           model: User,
           attributes: ["username"],
         },
-      ],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+
+      const post = dbPostData.get({ plain: true });
+      res.render("edit-post", { post, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
 });
 
 module.exports = router;
